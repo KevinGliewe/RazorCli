9d247084df3146c46fb2791931e534c38da896de
Kevin Gliewe <kevingliewe@outlook.de>
2018-11-20 00:56:26 +0100

    ⚡  Initial Commit

---
+++ src/RazorCli/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using RazorEngine;
+using RazorEngine.Templating;
+
+namespace RazorCli
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            CheckCommandLine(args);
+
+            string template = ReadFile(args[0]);
+            JObject model = ParseModel(args[1]);
+
+            var result = CompileTemplate(template, model);
+
+            Console.WriteLine(result);
+        }
+
+        private static string CompileTemplate(string template, JObject model)
+        {
+            string res = "";
+            try {
+                res = Engine.Razor.RunCompile(template, "templateKey", null, model);
+            } catch (RazorEngine.Templating.TemplateCompilationException ex) {
+                Console.WriteLine(ex);
+                System.Environment.Exit(1);
+            }
+            return res;
+        }
+
+        /* Cannot dispatch a dynamic object to extension methods */
+        private static JObject ParseModel(string fileName)
+        {
+            string json = ReadFile(fileName);
+            return JObject.Parse(json);
+        }
+
+        private static void CheckCommandLine(string[] args)
+        {
+            if (args.Length != 2) {
+                Usage();
+                System.Environment.Exit(1);
+            }
+        }
+
+        private static void Usage()
+        {
+            string usage = "Usage: razorcli <partial.cshtml> <model.json>\n";
+            Console.WriteLine(usage);
+        }
+
+        private static String ReadFile(string filename)
+        {
+            string result;
+
+            using (StreamReader sr = new StreamReader(filename)) {
+                result = sr.ReadToEnd();
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ src/build/Build.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.Git;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+class Build : NukeBuild
+{
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly string Configuration = IsLocalBuild ? "Debug" : "Release";
+
+    [Solution("RazorCli.sln")] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
+
+    AbsolutePath TestsDirectory => RootDirectory / "tests";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    Target Clean => _ => _
+        .Executes(() =>
+        {
+            DeleteDirectories(GlobDirectories(TestsDirectory, "**/bin", "**/obj"));
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
+
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.GetNormalizedAssemblyVersion())
+                .SetFileVersion(GitVersion.GetNormalizedFileVersion())
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore());
+        });
+    
+}
